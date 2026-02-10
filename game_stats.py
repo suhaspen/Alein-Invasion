6a90dcff1ec6cee3195b7d22cc08713ba2bbf971
suhaspen <suhaspendekanti@gmail.com>
2026-02-09 17:03:56 -0800

    Video Game

---
+++ game_stats.py
@@ -0,0 +1,10 @@
+class GameStats(): 
+    '''Tracking Game Statistics'''
+
+    def __init__(self, ai_game): 
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+    def reset_stats(self): 
+        self.ships_left = self.settings.ship_limit
+        
---
+++ settings.py
@@ -0,0 +1,22 @@
+class Settings:
+    def __init__(self):
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        
+        # Ship settings
+        self.ship_speed = 3
+        self.ship_limit = 3
+        
+        # Bullet settings
+        self.bullet_speed = 2.5
+        self.bullet_width = 3 
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+
+        #Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
